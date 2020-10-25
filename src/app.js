dc5c103818ee4849da27b00f0b7d5536dca4c872
Courtney Carson <cmacarson@gmail.com>
2020-10-25 16:13:20 -0700

    download dependencies, validate func

--- src/app.js
+++ src/app.js
@@ -15,10 +15,15 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
-
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken.split(' ')[0] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
 
 app.use(function errorHandler(error, req, res, next) {
   let response
