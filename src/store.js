956bf5be5ce0dcaa370bc42091ecbefcf17d39d5
Courtney Carson <cmacarson@gmail.com>
2020-10-25 16:18:14 -0700

    logger file, store

---
+++ src/store.js
@@ -0,0 +1,18 @@
+const bookmarks = [
+      {
+      'id': '1357adjaldfkjh',
+      'title': 'Curlsbot',
+      'url': 'https://www.curlsbot.com/',
+      'desc': 'Check product ingredients',
+      'rating': 5
+  }
+  {
+    'id': '158432158dsafad',
+    'title': 'Google',
+    'url': 'http://google.com',
+    'desc': 'where you google stuff',
+    'rating': 4
+  }
+]
+
+module.exports = bookmarks;
