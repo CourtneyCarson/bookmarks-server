683ac7fe170f9e34970c9172e981c14a6ccf6ae0
Courtney Carson <cmacarson@gmail.com>
2020-10-25 17:01:23 -0700

    add procfile, small edits on app

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const {NODE_ENV} = require('./config')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const logger = require('./logger');
 
-const app = express()
+const { NODE_ENV } = require('./config');
+const bookmarksRouter = require('./bookmarks/bookmarks-router');
+const bookmarks = require('./store');
+
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
@@ -24,6 +30,8 @@ app.use(function validateBearerToken(req, res, next) {
   }
   next();
 });
+
+app.use(bookmarksRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response
